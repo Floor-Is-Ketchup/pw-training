e46236ee2f4e417f85089e43f03fe75f4705c034
Floris Heins <floris.heins@rws.nl>
2026-05-22 15:27:17 +0200

    example screenshots

--- playwright/tests/screenshots/screenhots.example.spec.ts
+++ playwright/tests/screenshots/screenhots.example.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+//This testset shows how you can focus on locators and even edit styles for your screenshot testing.
+
 test.describe("actual tests", () => {
   test.beforeEach("test setup", async ({ page }) => {
     await page.goto("https://travels.praegus.nl");
@@ -10,13 +12,16 @@ test.describe("actual tests", () => {
     await expect(page).toHaveTitle(/Praegus Travels/);
   });
 
-  test("add campsite", async ({ page }) => {
+  test("landing page", async ({ page }) => {
     await expect(page).toHaveScreenshot();
   });
 
-  test("can't create a campsite without data", async ({ page }) => {
-    await expect(page.getByText("Featured Campsites$45/")).toHaveScreenshot();
+  test("campsite grid", async ({ page }) => {
+    await expect(page.getByText("Featured Campsites$45/")).toHaveScreenshot('grid.png');
   });
 
-  test("check title", async ({ page }) => {});
+  test("empty grid", async ({ page }) => {
+    await expect(page.getByText("Featured Campsites$45/")).toHaveScreenshot('empty-grid.png', {stylePath: 'playwright/tests/screenshots/empty_media.css'});
+    // await expect(page.getByText("Featured Campsites$45/")).toHaveScreenshot('empty-grid.png'); 
+  });
 });

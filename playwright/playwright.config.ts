b24109eb9253c3bfc3ea32d9bedd23e728e76683
Floris Heins <floris.heins@rws.nl>
2026-05-22 16:23:58 +0200

    kleine update

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], ignoreHTTPSErrors: true },
+      use: { ...devices["Desktop Chrome"], ignoreHTTPSErrors: true, headless: false },
     },
   ],
 });
--- playwright/tests/2-refactor/refactor.spec.ts
+++ playwright/tests/2-refactor/refactor.spec.ts
@@ -7,8 +7,6 @@ test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/Praegus Travels/);
 });
 
-
-
 test("get started link", async ({ page }) => {
   await page.goto(url);
   await page
@@ -17,30 +15,34 @@ test("get started link", async ({ page }) => {
     .click();
   // All the recorded actions for filling in the fields of a campsite have been moved to the function "fillCampsiteFields"
   // TODO: call the function here, with the right argument, to make the test pass again
-  await page.getByRole('button', { name: 'Create Campsite' }).click();
+  await page.getByRole("button", { name: "Create Campsite" }).click();
 
   // Bonus: change the following line to "expect" the text to be visible, instead of clicking it. turning this into an actual test!
   // You can find more information on this here: https://playwright.dev/docs/intro#installing-playwright
-  await page.getByText('Campsite created successfully').click();
+  await page.getByText("Campsite created successfully").click();
 });
 
 async function fillCampsiteFields(page: Page) {
-  await page.getByRole('textbox', { name: 'Name *' }).click();
-  await page.getByRole('textbox', { name: 'Name *' }).fill('a');
-  await page.getByRole('textbox', { name: 'Name *' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Location *' }).fill('b');
-  await page.getByRole('textbox', { name: 'Location *' }).press('Tab');
-  await page.getByLabel('Campsite Type *').press('ArrowDown');
-  await page.getByLabel('Campsite Type *').selectOption('tent');
-  await page.getByLabel('Campsite Type *').press('Tab');
-  await page.getByLabel('Terrain Type *').press('ArrowDown');
-  await page.getByLabel('Terrain Type *').selectOption('forest');
-  await page.getByLabel('Terrain Type *').press('Tab');
-  await page.getByRole('spinbutton', { name: 'Price per Night ($) *' }).fill('1');
-  await page.getByRole('spinbutton', { name: 'Price per Night ($) *' }).press('Tab');
-  await page.getByRole('spinbutton', { name: 'Capacity *' }).fill('1');
-  await page.getByRole('spinbutton', { name: 'Capacity *' }).press('Tab');
-  await page.getByLabel('Accessibility Level *').press('ArrowDown');
-  await page.getByLabel('Accessibility Level *').selectOption('easy');
-  await page.getByLabel('Accessibility Level *').press('Tab');
+  await page.getByRole("textbox", { name: "Name *" }).click();
+  await page.getByRole("textbox", { name: "Name *" }).fill("a");
+  await page.getByRole("textbox", { name: "Name *" }).press("Tab");
+  await page.getByRole("textbox", { name: "Location *" }).fill("b");
+  await page.getByRole("textbox", { name: "Location *" }).press("Tab");
+  await page.getByLabel("Campsite Type *").press("ArrowDown");
+  await page.getByLabel("Campsite Type *").selectOption("tent");
+  await page.getByLabel("Campsite Type *").press("Tab");
+  await page.getByLabel("Terrain Type *").press("ArrowDown");
+  await page.getByLabel("Terrain Type *").selectOption("forest");
+  await page.getByLabel("Terrain Type *").press("Tab");
+  await page
+    .getByRole("spinbutton", { name: "Price per Night ($) *" })
+    .fill("1");
+  await page
+    .getByRole("spinbutton", { name: "Price per Night ($) *" })
+    .press("Tab");
+  await page.getByRole("spinbutton", { name: "Capacity *" }).fill("1");
+  await page.getByRole("spinbutton", { name: "Capacity *" }).press("Tab");
+  await page.getByLabel("Accessibility Level *").press("ArrowDown");
+  await page.getByLabel("Accessibility Level *").selectOption("easy");
+  await page.getByLabel("Accessibility Level *").press("Tab");
 }

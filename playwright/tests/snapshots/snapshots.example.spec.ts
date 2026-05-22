21f0661b0fed9089ae4030a04bda7d2a5f68a42e
Floris Heins <floris.heins@rws.nl>
2026-05-22 15:43:50 +0200

    added snapshots

---
+++ playwright/tests/snapshots/snapshots.example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+// Using the aria / accessibility tree, you can also perform checks
+
+test.describe("snapshot tests", () => {
+  test.beforeEach("test setup", async ({ page }) => {
+    await page.goto("https://travels.praegus.nl");
+  });
+
+  test("has title", async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Praegus Travels/);
+  });
+
+  test("landing page", async ({ page }) => {
+    await expect(page).toMatchAriaSnapshot()
+  });
+
+  test("grid with yaml", async ({ page }) => {
+    await expect(page.getByText("Featured Campsites$45/")).toMatchAriaSnapshot({name: 'grid.yml'});
+  });
+
+  test("grid with inline", async ({ page }) => {
+    await expect(page.getByText("Featured Campsites$45/")).toMatchAriaSnapshot('- heading "Featured Campsites" [level=2]');
+  });
+
+});
+
